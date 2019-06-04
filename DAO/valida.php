ce48d79dcdaa39afa2f87b32b9cb1bae110b8717
la1608 <laisvieira1608@gmail.com>
2019-06-04 16:37:20 -0300

    3 commit - Login,valida

---
+++ DAO/valida.php
@@ -0,0 +1,77 @@
+<?php
+		include "conexao.php";
+
+	$acesso =$_POST['Tipo'];
+	$usuario = $_POST['usuario'];
+	$senha = $_POST['senha'];
+
+	if ($acesso==0){
+		$sql = "SELECT * FROM candidato WHERE email = '$usuario' or cpf ='$usuario' and senha = '$senha' ";
+		
+		$res = $con->query($sql);
+		$linha = $res->fetch_assoc();
+	
+	$id = $linha['idCandidato'];
+	$nome = $linha['nome'];
+	$user = $linha['email'];
+	$pass = $linha['senha'];
+	$user2 = $linha['cpf'];
+	
+	if (($usuario == $user || $usuario == $user2 ) && $senha == $pass && $acesso == 0)
+
+	{
+	
+		session_start();
+		$_SESSION['idCandidato'] = $id;
+		$_SESSION['nome'] = $nome;
+		$_SESSION['user'] = $user;
+	
+		header('location: ../indexCandidato.php'); 
+	}
+
+	else
+
+	{
+		header('location: ../loguin.php');
+	}
+	
+
+	}
+
+ else if($acesso==1){
+
+
+$sql = "SELECT * FROM professor WHERE email = '$usuario' or cpf ='$usuario' and senha = '$senha' ";
+
+	$res = $con->query($sql);
+	$linha = $res->fetch_assoc();
+
+$id = $linha['idProfessor'];
+$nome = $linha['nome'];
+$user = $linha['email'];
+$pass = $linha['senha'];
+$tipo = $linha['perfilUsuario'];
+$user2 = $linha['cpf'];
+
+if (($usuario == $user || $usuario == $user2 ) && $senha == $pass && ($tipo == 1 || $tipo == 2))
+
+{
+
+	session_start();
+    $_SESSION['idProfessor'] = $id;
+    $_SESSION['nome'] = $nome;
+	$_SESSION['user'] = $user;
+
+	header('location: ../indexProfessor.php'); 
+}
+
+
+else
+
+{
+	header('location: ../loguin.php');
+}
+
+ }
+
+?>
--- loguin.php
+++ loguin.php
@@ -10,16 +10,16 @@
     <title>Processo Seletivo | </title>
 
     <!-- Bootstrap -->
-    <link href="bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <!-- NProgress -->
+    <link href="CSS/bootstrap.min.css" rel="stylesheet">
+   <!-- Font Awesome -->
+    <!--  <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+     NProgress 
     <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
-    <!-- Animate.css -->
-    <link href="../vendors/animate.css/animate.min.css" rel="stylesheet">
+  Animate.css 
+    <link href="../vendors/animate.css/animate.min.css" rel="stylesheet">-->
 
     <!-- Custom Theme Style -->
-    <link href="custom.min.css" rel="stylesheet">
+    <link href="CSS/custom.min.css" rel="stylesheet">
   </head>
 
   <body class="login">
@@ -30,19 +30,20 @@
       <div class="login_wrapper">
         <div class="animate form login_form">
           <section class="login_content">
-            <form action="valida.php" method="post">
+            <form action="DAO/valida.php" method="post">
               <h1>Login</h1>
               <div>
-                <input type="text" class="form-control" placeholder="Digite seu CPF ou Email" required="" />
+                <input type="text" class="form-control" placeholder="Digite seu CPF ou Email" required="" name="usuario"/>
               </div>
                 <div>
-                <input type="password" class="form-control" placeholder="Senha" required="" />
+                <input type="password" class="form-control" placeholder="Senha" required="" name="senha"/>
                 </div>
                     <div>
                         <label class="form-control">Tipo de usuário</label>
-                <select class="form-control" id=selectTipoPerfil>
-    <option name="professor">Professor</option>
-    <option name="candidato">Candidato</option>
+                <select class="form-control" id=selectTipoPerfil name="Tipo">
+    <option value="1">Professor</option>
+    <option value="0">Candidato</option>
+
 </select>
 </div>
         
