251887b096031ece3a1ddbdbe622a1b465328a0f
la1608 <laisvieira1608@gmail.com>
2019-07-02 15:00:51 -0300

    18 commit- atualizado

--- administrador/envioCurso.php
+++ administrador/envioCurso.php
@@ -2,10 +2,21 @@
 
 include_once "../DAO/conexao.php";
 
+$result_consultaCurso= "SELECT * FROM curso ";
+
+$res = $con-> query($result_consultaCurso);
+$linha = $res->fetch_assoc();
+
 $nome = $_POST["nome"];
 $sql = "INSERT INTO curso (nomeCurso) VALUES ('$nome')";
 
-if ($con->query($sql) === TRUE){
+if ($linha['nomeCurso'] = $nome){
+
+echo "<script>alert('Curso já existente! Cadastre um curso novo');window.location='CadastroCurso.php'</script>";
+
+}
+
+else if ($con->query($sql) === TRUE){
 
 	echo "<script>alert('Cadastro realizado com sucesso!');window.location='CadastroCurso.php'</script>";
 } else {
